0d0acc05a4904c30c0f4f94a7436a746fc30eaf7
Timothy Smith <timmy.smith@gmx.net>
2016-12-09 11:08:54 +0100

    dropout

--- MAIN.py
+++ MAIN.py
@@ -132,20 +132,20 @@ if __name__ == '__main__':
     # cuts down input size going into RNN:
     inner = TimeDistributed(Dense(time_dense_size, activation=act, name='dense1'))(inner)
 
-    # # Dropout
-    # inner = Dropout(0.1)(inner) # TODO
+    # Dropout
+    inner = Dropout(0.3)(inner) # TODO
 
     # RNN
     # Two layers of bidirecitonal GRUs
     # GRU seems to work as well, if not better than LSTM:
-    gru_1 = GRU(rnn_size, return_sequences=True, name='gru1', dropout_W=0.2, dropout_U=0.2)(inner)# TODO
-    gru_1b = GRU(rnn_size, return_sequences=True, go_backwards=True, name='gru1_b', dropout_W=0.2, dropout_U=0.2)(inner)# TODO
+    gru_1 = GRU(rnn_size, return_sequences=True, name='gru1')(inner)# TODO
+    gru_1b = GRU(rnn_size, return_sequences=True, go_backwards=True, name='gru1_b')(inner)# TODO
     gru1_merged = merge([gru_1, gru_1b], mode='sum')
-    gru_2 = GRU(rnn_size, return_sequences=True, name='gru2', dropout_W=0.2, dropout_U=0.2)(gru1_merged)# TODO
-    gru_2b = GRU(rnn_size, return_sequences=True, go_backwards=True, name='gru2_b', dropout_W=0.2, dropout_U=0.2)(gru1_merged)# TODO
+    gru_2 = GRU(rnn_size, return_sequences=True, name='gru2')(gru1_merged)# TODO
+    gru_2b = GRU(rnn_size, return_sequences=True, go_backwards=True, name='gru2_b')(gru1_merged)# TODO
 
     # transforms RNN output to character activations:
-    inner = TimeDistributed(Dense(output_size + 1, name='dense2'))(merge([gru_2, gru_2b], mode='concat'))
+    inner = TimeDistributed(Dense(output_size + 1, name='dense2'))(merge([gru_2, gru_2b], mode='concat')) # mode='concat'))
     y_pred = Activation('softmax', name='softmax')(inner)
     Model(input=[input_data], output=y_pred).summary()
 
