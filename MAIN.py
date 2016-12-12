07e6782b8815238443464f13d19909a447e1de59
Timothy Smith <timmy.smith@gmx.net>
2016-12-12 13:33:04 +0100

    300regl2

--- MAIN.py
+++ MAIN.py
@@ -15,7 +15,7 @@ from keras.utils.visualize_util import plot
 import Config.char_alphabet as char_alpha
 import Tools.InputGenerator as InputGenerator
 import Tools.ReporterCallback as ReporterCallback
-from keras.regularizers import l1, l2
+from keras.regularizers import l2
 
 
 # the actual loss calc occurs here despite it not being
@@ -140,11 +140,11 @@ if __name__ == '__main__':
     # RNN
     # Two layers of bidirecitonal GRUs
     # GRU seems to work as well, if not better than LSTM:
-    gru_1 = LSTM(rnn_size, return_sequences=True, name='gru1', W_regularizer=l1(0.01), U_regularizer=l1(0.01), b_regularizer=l1(0.01))(inner)# TODO
-    gru_1b = LSTM(rnn_size, return_sequences=True, go_backwards=True, name='gru1_b', W_regularizer=l1(0.01), U_regularizer=l1(0.01), b_regularizer=l1(0.01))(inner)# TODO
+    gru_1 = LSTM(rnn_size, return_sequences=True, name='gru1', W_regularizer=l2(0.01), U_regularizer=l2(0.01), b_regularizer=l2(0.01))(inner)# TODO
+    gru_1b = LSTM(rnn_size, return_sequences=True, go_backwards=True, name='gru1_b', W_regularizer=l2(0.01), U_regularizer=l2(0.01), b_regularizer=l2(0.01))(inner)# TODO
     gru1_merged = merge([gru_1, gru_1b], mode='sum')
-    gru_2 = LSTM(rnn_size, return_sequences=True, name='gru2', W_regularizer=l1(0.01), U_regularizer=l1(0.01), b_regularizer=l1(0.01))(gru1_merged)# TODO
-    gru_2b = LSTM(rnn_size, return_sequences=True, go_backwards=True, name='gru2_b', W_regularizer=l1(0.01), U_regularizer=l1(0.01), b_regularizer=l1(0.01))(gru1_merged)# TODO
+    gru_2 = LSTM(rnn_size, return_sequences=True, name='gru2', W_regularizer=l2(0.01), U_regularizer=l2(0.01), b_regularizer=l2(0.01))(gru1_merged)# TODO
+    gru_2b = LSTM(rnn_size, return_sequences=True, go_backwards=True, name='gru2_b', W_regularizer=l2(0.01), U_regularizer=l2(0.01), b_regularizer=l2(0.01))(gru1_merged)# TODO
 
     # transforms RNN output to character activations:
     inner = TimeDistributed(Dense(output_size + 1, name='dense2'))(merge([gru_2, gru_2b], mode='concat')) # mode='concat'))
